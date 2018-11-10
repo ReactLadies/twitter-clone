da88cdba4330eebff0093a11ea6382942c68d1c5
mrayana <https://github.com/mrayana>
2018-11-09 23:47:42 -0300

    Add package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
 	render () {
 		return (
 			<div className="App">
-				<h1>Twitter Clone</h1>
+			<div className="App-Header">
+				<h1>Header</h1>
+			</div>
+			<div className="App-Profile">
+				<h2>Profile</h2>
+			</div>
+			<div className="App-Trends">
+				<h2>Trends</h2>
+			</div>
+			<div className="App-NewCard">
+				<h2>NewCard</h2>
+			</div>
+			<div className="App-Card">
+				<h2>Card</h2>
+			</div>
+			<div className="App-Follow">
+				<h2>Follow</h2>
+			</div>
 			</div>
 		);
 	}
